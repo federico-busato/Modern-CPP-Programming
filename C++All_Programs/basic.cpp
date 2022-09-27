7a45c1bb7ea4555e256df7673cd812fbda6a2e4a
Sanya Verma <sanyaverma99789@gmail.com>
2022-09-27 18:29:20 +0530

    Basics1

---
+++ C++All_Programs/basic.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+ // 1. input ans output
+    int n;
+
+    cout<<"Enter a number : ";
+    cin>>n;
+    cout<<"\n Entered number is ---> "<<n;
+
+
+
+
+
+
+    return 0;
+}
